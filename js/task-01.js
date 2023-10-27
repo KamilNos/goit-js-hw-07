1bf8ee6c6e9dc6f84d26640ea160fa50e4fd9185
KamilNos <128514595+KamilNos@users.noreply.github.com>
2023-10-27 15:44:59 +0200

    Add files via upload

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoryList = document.querySelector("#categories");
+const categoryItems = categoryList.querySelectorAll("li.item");
+for (const item of categoryItems) {
+  const title = item.querySelector("h2").textContent;
+  const categoryElements = item.querySelectorAll("ul li");
+  const numberOfElements = categoryElements.length;
+  console.log("Category: " + title);
+  console.log("Elements: " + numberOfElements);
+}
